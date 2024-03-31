d0f9cf417d4b08785e5203433f09c6d5ca5f3846
Geeten <geeten2301@gmail.com>
2024-03-31 13:21:04 +0530

    ui design

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,15 @@
+import { BiLogOut } from "react-icons/bi";
+
+
+const LogoutButton = () => {
+
+	return (
+		<div className='mt-auto'>
+			{/*  */}
+			     <div className='divider my-0 py-0 h-5' />
+				<BiLogOut className='w-6 h-6 text-white cursor-pointer'  />
+			
+		</div>
+	);
+};
+export default LogoutButton;
